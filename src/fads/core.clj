0944e34829111117c38c59bbfa906b0d2a0def22
Paul Ingles <paul@oobaloo.co.uk>
2015-09-14 16:20:13 +0100

    follow paging cursors to concat data together

--- src/fads/core.clj
+++ src/fads/core.clj
@@ -2,9 +2,20 @@
   (:require [clj-facebook-graph.client :as fb]
             [clj-facebook-graph.auth :as fba]))
 
+(defn account-insights [account-id params]
+  (fb/get ["v2.4" (str "act_" account-id) "insights"]
+          {:query-params params}))
+
 (defn insights [token account-id]
   (fba/with-facebook-auth {:access-token token}
-    (fb/get ["v2.4" (str "act_" account-id) "insights"]
-            {:query-params {:level "adgroup"
-                            :date_preset "yesterday"
-                            :fields "adgroup_name,campaign_name,clicks,spend,impressions"}})))
+    (let [params {:level "adgroup"
+                  :date_preset "yesterday"
+                  :fields "adgroup_name,campaign_name,clicks,spend,impressions"}]
+      (loop [all      nil
+             response (account-insights account-id params)]
+        (let [{:keys [data paging]} (:body response)
+              after                 (get-in paging [:cursors :after])]
+          (if (nil? after)
+            all
+            (recur (concat all data)
+                   (account-insights account-id (assoc params :after after)))))))))
